1012a2b2001cea8e093f0822e0f52aa73338403d
dilwashk <dkha0005@student.monash.edu>
2019-10-07 13:45:24 +0200

    Commit initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,91 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
+
   selector: 'app-root',
+
   templateUrl: './app.component.html',
+
   styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
-  title = 'library';
+
+  data = [];
+
+  title = 'Week9';
+
+  name = '';
+
+  date = '';
+
+  status = '';
+
+  summary = '';
+
+ hardCoverDeleted = 0;
+
+  totalBooks = 0;
+
+ // hardCoverCounter = 0;
+
+
+
+  newItem() {
+
+    this.data.push({name: this.name, date: this.date, status: this.status, summary: this.summary});
+    this.totalBooks++;
+
+    if(this.status == 'hardCover'){
+      this.hardCoverDeleted++;
+
+
+    }
+  }
+
+  deleteItem(name) {
+
+    for (let i = 0; i < this.data.length; i++) {
+
+
+  if (this.data[i].name == name  && this.data[i].status == 'hardCover' ) {
+  this.totalBooks--;
+  this.hardCoverDeleted--;
+  this.data.splice(i, 1);
+  break;
 }
+  if (this.data[i].name == name) {
+     this.totalBooks --;
+     this.data.splice(i, 1);
+     break;
+     }
+
+
+
+
+  }
+  }
+
+
+  deleteHardCover() {
+
+   for (let i = 0; i < this.data.length; i++) {
+    if (this.data[i].status == 'hardCover') {
+
+      this.data.splice(i, this.hardCoverDeleted);
+      this.totalBooks = this.totalBooks - this.hardCoverDeleted ;
+      this.hardCoverDeleted -= this.hardCoverDeleted;
+    }
+
+    }
+
+         }
+    }
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+
+import {FormsModule} from '@angular/forms';
+
+
+
 
 @NgModule({
+
   declarations: [
+
     AppComponent
+
   ],
+
   imports: [
+
     BrowserModule,
-    AppRoutingModule
+
+    FormsModule
+
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
